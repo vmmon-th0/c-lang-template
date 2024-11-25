bf8b711277003592911419d5c27fc88e45869f1f
Ammon <60857611+vmmon-th0@users.noreply.github.com>
2024-11-25 21:15:56 +0100

    Update template.h

--- include/template.h
+++ include/template.h
@@ -1,4 +1,12 @@
 #ifndef TEMPLATE_H
 #define TEMPLATE_H
 
+#ifdef DEBUG
+#define TEMPLATE_DEBUG(fmt, ...)                                                   \
+    fprintf (stderr, "DEBUG: %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__,  \
+             ##__VA_ARGS__)
+#else
+#define TEMPLATE_DEBUG(fmt, ...)
+#endif
+
 #endif
